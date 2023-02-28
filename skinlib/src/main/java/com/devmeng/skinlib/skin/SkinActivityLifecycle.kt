035ab33788a6d841fb3fc5238b3a35ad42b65a21
devmeng <william278@163.com>
2023-02-28 18:18:50 +0800

    更新 LayoutInflater Factory2 的初始化位置

--- skinlib/src/main/java/com/devmeng/skinlib/skin/SkinActivityLifecycle.kt
+++ skinlib/src/main/java/com/devmeng/skinlib/skin/SkinActivityLifecycle.kt
@@ -1,10 +1,8 @@
 package com.devmeng.skinlib.skin
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
 import android.graphics.Typeface
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.devmeng.skinlib.skin.utils.Log
@@ -20,18 +18,15 @@ import java.lang.ref.WeakReference
  * 并且对每一个 Factory 进行观察者注册
  *
  */
-class SkinActivityLifecycle(val activities: MutableList<String>) :
+class SkinActivityLifecycle(private val activities: MutableList<String>) :
     Application.ActivityLifecycleCallbacks {
 
     private val factoryMap: HashMap<Activity, SkinLayoutFactory> = hashMapOf()
 
-    @SuppressLint("DiscouragedPrivateApi")
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+    override fun onActivityPreCreated(activity: Activity, savedInstanceState: Bundle?) {
         if (singleApplySkin(activity)) {
             return
         }
-        //防止重启状态栏还原
-//        SkinThemeUtils.updateStatusBarState(activity)
         //加载皮肤包 字体
         var skinTypeface: Typeface? = null
         val layoutInflater = LayoutInflater.from(activity)
@@ -42,23 +37,15 @@ class SkinActivityLifecycle(val activities: MutableList<String>) :
                 Log.e(e.stackTraceToString())
             }
         }
-
-        try {
-            //根据源码中 setFactory2 方法需将 mFactorySet 先设置成 false，才可使用
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-                val field = LayoutInflater::class.java.getDeclaredField("mFactorySet")
-                field.isAccessible = true
-                field.setBoolean(layoutInflater, false)
-            }
-
-        } catch (e: Exception) {
-            Log.e(e.stackTraceToString())
-        }
         val factory = SkinLayoutFactory(activity, skinTypeface)
         layoutInflater.factory2 = factory
         //注册观察者
         SkinManager.instance.addObserver(factory)
         factoryMap[activity] = factory
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+
     }
 
     override fun onActivityStarted(activity: Activity) {
